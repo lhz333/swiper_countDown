1ac81f5ccc50ef39e189520d5a868afa441d7d72
lihuizhen <huizhen.li@charleskeith.com>
2020-03-25 15:54:00 +0800

    添加swiper

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,57 @@
 <template>
   <div class="hello">
     <h3>swiper中使用倒计时</h3>
-    <count-down
-      :endDate="endDate"
-    ></count-down>
+    <swiper ref="swiper" class="swiper" :options="swiperOption">
+      <swiper-slide class="slide" v-for="(item, index) in arr" :key="index">
+        <count-down :endDate="item.endDate"></count-down>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 import CountDown from './CountDown'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 export default {
   name: 'HelloWorld',
-  data() {
+  data () {
     return {
-      endDate: Date.parse(new Date()) + 2000000000
+      arr: [{
+        endDate: Date.parse(new Date()) + 100000000
+      }, {
+        endDate: Date.parse(new Date()) + 1000000000
+      }, {
+        endDate: Date.parse(new Date()) + 2000000000
+      }],
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        loop: true,
+        observer: true,
+        observeParents: true,
+        preventLinksPropagation: true,
+        paginationClickable: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false
+        },
+        on: {
+          // 回调函数，swiper从一个slide过渡到另一个slide结束时执行。
+          slideChangeTransitionEnd () {
+            // 在Loop模式下Swiper切换到指定slide。切换到的是slide索引是realIndex
+            this.slideToLoop(this.realIndex, 0, false)
+          }
+        }
+      }
     }
   },
   components: {
-    CountDown
+    CountDown,
+    swiper,
+    swiperSlide
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+// 引入vue-awesome-swiper
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/dist/css/swiper.css';
+Vue.use (VueAwesomeSwiper);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue ({
+  render: h => h (App),
+}).$mount ('#app');
